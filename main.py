562d32dd42ba614dfb51b59199b4f7a90008446e
marl <76800212+LordBartholomew@users.noreply.github.com>
2020-12-30 22:40:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ logging.basicConfig(filename="main.log", level=logging.DEBUG)
 
 ## static var
 id=-1
-def trash_way_to_make_static_var():
+def newid():
 	global id
 	id += 1
 	return id
 
 class service():
 	def __init__(self):
-		self.ID = trash_way_to_make_static_var()
+		self.ID = newid()
 		self.LHOST = None
 		self.LPORT = None
 
@@ -60,6 +60,13 @@ class service():
 			sock.bind((self.LHOST, self.LPORT))
 			sock.listen(2)
 
+		except Exception as e:
+			sock.close()
+			logging.error("{0}:{1}".format(self.ID, e))
+			print("{0}:{1}".format(self.ID, e))
+			return
+
+		try:
 			logging.info("{0}:Starting Server on Port {1}.".format(self.ID, self.LPORT))
 			print("{0}:Starting Server on Port {1}.".format(self.ID, self.LPORT))
 
@@ -73,7 +80,7 @@ class service():
 		except Exception as e:
 			logging.error("{0}:{1}".format(self.ID, e))
 			print("{0}:{1}".format(self.ID, e))
-		
+
 		finally:
 			sock.close()
 
@@ -83,20 +90,19 @@ def main():
 	serv = list()
 	serv.append(service())
 	serv.append(service())
-	serv.append(service())
-	serv.append(service())
 
 	serv[0].setLHOST("127.0.0.1")
 	serv[0].setLPORT(21)
 
 	serv[1].setLHOST("127.0.0.1")
 	serv[1].setLPORT(22)
-
+	
 	serv[2].setLHOST("127.0.0.1")
-	serv[2].setLPORT(443)
-
+	serv[2].setLPORT(80)
+	
 	serv[3].setLHOST("127.0.0.1")
-	serv[3].setLPORT(4444)
+	serv[3].setLPORT(443)
+
 
 	for i in range(len(serv)):
 		thread = Thread(target=serv[i].run, args=())
